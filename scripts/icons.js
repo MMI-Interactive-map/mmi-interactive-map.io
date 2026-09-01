8337e8fd859aafb1e8b8d8d35c61887a91f9d2f6
Eppe <eppe.maitre@gmail.com>
2026-09-01 20:04:04 +0200

    Transfer of public onto the root

--- scripts/icons.js
+++ scripts/icons.js
@@ -1,72 +1,72 @@
 export let icons = {
     test_icon: L.icon({
-        iconUrl: '../scripts/icons/test_icon.png',
+        iconUrl: './scripts/icons/test_icon.png',
         iconSize: [24, 24],
         iconAnchor: [10, 10],
-        shadowUrl: '../scripts/icons/test_icon_shadow.png',
+        shadowUrl: './scripts/icons/test_icon_shadow.png',
         shadowSize: [24, 24],
         shadowAnchor: [10, 9]
     }),
 
     store_icon: L.icon({
-        iconUrl: '../scripts/icons/store.png',
+        iconUrl: './scripts/icons/store.png',
         iconSize: [24, 24],
         iconAnchor: [10, 10],
-        shadowUrl: '../scripts/icons/test_icon_shadow.png',
+        shadowUrl: './scripts/icons/test_icon_shadow.png',
         shadowSize: [24, 24],
         shadowAnchor: [10, 9]
     }),
 
     restaurant_icon: L.icon({
-        iconUrl: '../scripts/icons/restaurant.png',
+        iconUrl: './scripts/icons/restaurant.png',
         iconSize: [24, 24],
         iconAnchor: [10, 10],
-        shadowUrl: '../scripts/icons/test_icon_shadow.png',
+        shadowUrl: './scripts/icons/test_icon_shadow.png',
         shadowSize: [24, 24],
         shadowAnchor: [10, 9]
     }),
 
     administation_icon: L.icon({
-        iconUrl: '../scripts/icons/adminisatrion.png',
+        iconUrl: './scripts/icons/adminisatrion.png',
         iconSize: [24, 24],
         iconAnchor: [10, 10],
-        shadowUrl: '../scripts/icons/test_icon_shadow.png',
+        shadowUrl: './scripts/icons/test_icon_shadow.png',
         shadowSize: [24, 24],
         shadowAnchor: [10, 9]
     }),
 
     bank_icon: L.icon({
-        iconUrl: '../scripts/icons/banks.png',
+        iconUrl: './scripts/icons/banks.png',
         iconSize: [24, 24],
         iconAnchor: [10, 10],
-        shadowUrl: '../scripts/icons/test_icon_shadow.png',
+        shadowUrl: './scripts/icons/test_icon_shadow.png',
         shadowSize: [24, 24],
         shadowAnchor: [10, 9]
     }),
 
     medical_icon: L.icon({
-        iconUrl: '../scripts/icons/medical.png',
+        iconUrl: './scripts/icons/medical.png',
         iconSize: [24, 24],
         iconAnchor: [10, 10],
-        shadowUrl: '../scripts/icons/test_icon_shadow.png',
+        shadowUrl: './scripts/icons/test_icon_shadow.png',
         shadowSize: [24, 24],
         shadowAnchor: [10, 9]
     }),
 
     activity_icon: L.icon({
-        iconUrl: '../scripts/icons/activity.png',
+        iconUrl: './scripts/icons/activity.png',
         iconSize: [24, 24],
         iconAnchor: [10, 10],
-        shadowUrl: '../scripts/icons/test_icon_shadow.png',
+        shadowUrl: './scripts/icons/test_icon_shadow.png',
         shadowSize: [24, 24],
         shadowAnchor: [10, 9]
     }),
 
     partner_icon: L.icon({
-        iconUrl: '../scripts/icons/partner.png',
+        iconUrl: './scripts/icons/partner.png',
         iconSize: [24, 24],
         iconAnchor: [10, 10],
-        shadowUrl: '../scripts/icons/test_icon_shadow.png',
+        shadowUrl: './scripts/icons/test_icon_shadow.png',
         shadowSize: [24, 24],
         shadowAnchor: [10, 9]
     })
